fd0f3e9040f9e9a28d4da79ccd1da20815980c21
SYARGEY <lss1895@mail.ru>
2023-02-19 23:02:11 +0300

    Семинар 8, задание 2

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 2: Задайте прямоугольный двумерный массив. Напишите программу,
+// которая будет находить строку с наименьшей суммой элементов.
+int[,] GenerateArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array) // печатаем массив
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+int PoiskMinSum (int[,] array)
+{
+    int str = 0;
+    int minSum = int.MaxValue;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (sum < minSum)
+        {
+            minSum = sum;
+            str = i;
+        }
+    }
+    return str + 1;
+}
+int[,] matrix = GenerateArray(3, 3);
+PrintArray(matrix);
+System.Console.WriteLine($"Строка с минимальной суммой - номер {PoiskMinSum(matrix)}");
